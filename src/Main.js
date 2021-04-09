7582b073c0ca1cf172bbc5b477e61ef83e52f654
furyx <furyx@hotmail.com>
2021-04-09 17:45:48 +0800

    chapter 22

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+
+import Screens from './screens';
 
 const Main = () => {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello world!</Text>
-    </View>
-  );
+  return  <Screens />;
+  // (
+    // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    //   <Text style={{ color: '#0077cc', fontSize: 48, fontWeight: 'bold' }}>
+    //     Hello world!!!
+    //   </Text>
+    //   <Image source={require('../assets/images/hello-world.jpg')} />
+    // </View>
+  // );
 };
 
 export default Main;
